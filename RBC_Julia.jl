d43cd5d5784020c8131cd115ec1f93ec67b2f40a
jesusfv <jesusfv@econ.upenn.edu>
2014-06-13 22:30:29 -0400

    Update RBC_Julia.jl
    Now types are defined.
    
    Thanks to Florian Oswald for point out the importance of type definition.

--- RBC_Julia.jl
+++ RBC_Julia.jl
@@ -4,7 +4,31 @@
 # Haverford, July 29, 2013
 
 function main()
+    
+    # define types
+    # types are not needed but they improve performance considerably
 
+    aalpha :: Float64
+    bbeta  :: Float64
+
+    vProductivity ::Array{Float64,2}
+    mTransition   ::Array{Float64,2}
+
+    capitalSteadyState     :: Float64
+    outputSteadyState      :: Float64
+    consumptionSteadyState :: Float64
+
+    vGridCapital :: Array{Float64,1}
+
+    nGridCapital :: Int64
+    nGridProductivity :: Int64
+
+    mOutput               :: Array{Float64,2}
+    mValueFunction        :: Array{Float64,2}
+    mValueFunctionNew     :: Array{Float64,2}
+    mPolicyFunction       :: Array{Float64,2}
+    expectedValueFunction :: Array{Float64,2}
+    
     ##  1. Calibration
 
     aalpha = 1/3     # Elasticity of output w.r.t. capital
