81bbf08f5b59955fe46c00e47bd49750b2aaa539
Kevin Sheppard <kevin.sheppard@economics.ox.ac.uk>
2014-07-16 17:41:46 +0100

    Added Cython version which wraps both the inner and outer loops

--- innerloop_ext.pyx
+++ innerloop_ext.pyx
@@ -26,16 +26,16 @@ cdef extern from "math.h":
 # local variables
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def innerloop(double bbeta,
+cpdef innerloop(double bbeta,
               int nGridCapital,
               int gridCapitalNextPeriod,
-              np.ndarray[np.float64_t, ndim=2] mOutput not None,
+              np.ndarray[np.float64_t, ndim=2] mOutput,
               int nProductivity,
-              np.ndarray[np.float64_t, ndim=1] vGridCapital not None,
-              np.ndarray[np.float64_t, ndim=2] expectedValueFunction not None,
-              np.ndarray[np.float64_t, ndim=2] mValueFunction not None,
-              np.ndarray[np.float64_t, ndim=2] mValueFunctionNew not None,
-              np.ndarray[np.float64_t, ndim=2] mPolicyFunction not None):
+              np.ndarray[np.float64_t, ndim=1] vGridCapital,
+              np.ndarray[np.float64_t, ndim=2] expectedValueFunction,
+              np.ndarray[np.float64_t, ndim=2] mValueFunction,
+              np.ndarray[np.float64_t, ndim=2] mValueFunctionNew,
+              np.ndarray[np.float64_t, ndim=2] mPolicyFunction):
 
     cdef int nCapital, nCapitalNextPeriod
     cdef double valueHighSoFar, capitalChoice, consumption, valueProvisional
@@ -59,3 +59,55 @@ def innerloop(double bbeta,
         mPolicyFunction[nCapital,nProductivity]   = capitalChoice
 
     return mValueFunctionNew, mPolicyFunction
+
+
+
+# Wrap the two inner most loops
+# Additional Data Types
+#                           Python                 Cython
+# - nGridProductivity:      int64                  int
+# - nGridCapital:           int64                  int
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cpdef outerloop(double bbeta,
+              int nGridProductivity,
+              int nGridCapital,
+              np.ndarray[np.float64_t, ndim=2] mOutput,
+              np.ndarray[np.float64_t, ndim=1] vGridCapital,
+              np.ndarray[np.float64_t, ndim=2] expectedValueFunction,
+              np.ndarray[np.float64_t, ndim=2] mValueFunction,
+              np.ndarray[np.float64_t, ndim=2] mValueFunctionNew,
+              np.ndarray[np.float64_t, ndim=2] mPolicyFunction):
+
+    cdef int nProductivity, gridCapitalNextPeriod, nCapital, nCapitalNextPeriod
+    cdef double valueHighSoFar, capitalChoice, consumption, valueProvisional
+
+    for nProductivity in xrange(nGridProductivity):
+
+        # We start from previous choice (monotonicity of policy function)
+        gridCapitalNextPeriod = 0
+
+        for nCapital in xrange(nGridCapital):
+
+            valueHighSoFar = -100000.0
+            capitalChoice  = vGridCapital[0]
+
+            for nCapitalNextPeriod in xrange(gridCapitalNextPeriod,nGridCapital):
+
+                consumption = mOutput[nCapital,nProductivity] - vGridCapital[nCapitalNextPeriod]
+
+                valueProvisional = (1-bbeta)*log(consumption)+bbeta*expectedValueFunction[nCapitalNextPeriod,nProductivity];
+
+                if  valueProvisional>valueHighSoFar:
+                    valueHighSoFar = valueProvisional
+                    capitalChoice = vGridCapital[nCapitalNextPeriod]
+                    gridCapitalNextPeriod = nCapitalNextPeriod
+                else:
+                    break # We break when we have achieved the max
+
+
+            mValueFunctionNew[nCapital,nProductivity] = valueHighSoFar
+            mPolicyFunction[nCapital,nProductivity]   = capitalChoice
+
+    return mValueFunctionNew
