5d21d60cc15237c74fc186be45a3f3047b21f9c6
Andreas Noack <andreasnoackjensen@gmail.com>
2017-03-22 13:11:02 -0400

    Add a faster devectorized Julia version

---
+++ RBC_Julia_2.jl
@@ -0,0 +1,130 @@
+## Basic RBC model with full depreciation
+#
+# Jesus Fernandez-Villaverde
+# Haverford, July 29, 2013
+
+## Load package with optimized distance functions
+using Distances
+@static if is_apple()
+    ## Use the system log function which is 2x faster on macOS
+    mylog(x::Float64)  = ccall((:log, :libm), Float64, (Float64,), x)
+else
+    mylog(x::Float64) = log(x)
+end
+
+function main()
+
+    ##  1. Calibration
+
+    aalpha = 1/3     # Elasticity of output w.r.t. capital
+    bbeta  = 0.95    # Discount factor
+
+    # Productivity values
+    vProductivity = [0.9792 0.9896 1.0000 1.0106 1.0212]
+
+    # Transition matrix
+    mTransition   = [0.9727 0.0273 0.0000 0.0000 0.0000;
+                     0.0041 0.9806 0.0153 0.0000 0.0000;
+                     0.0000 0.0082 0.9837 0.0082 0.0000;
+                     0.0000 0.0000 0.0153 0.9806 0.0041;
+                     0.0000 0.0000 0.0000 0.0273 0.9727]
+
+    # 2. Steady State
+
+    capitalSteadyState = (aalpha*bbeta)^(1/(1-aalpha))
+    outputSteadyState = capitalSteadyState^aalpha
+    consumptionSteadyState = outputSteadyState-capitalSteadyState
+
+    println("Output = ",outputSteadyState," Capital = ",capitalSteadyState," Consumption = ",consumptionSteadyState)
+
+    # We generate the grid of capital
+    vGridCapital = collect(0.5*capitalSteadyState:0.00001:1.5*capitalSteadyState)
+
+    nGridCapital = length(vGridCapital)
+    nGridProductivity = length(vProductivity)
+
+    # 3. Required matrices and vectors
+
+    mOutput           = zeros(nGridCapital,nGridProductivity)
+    mValueFunction    = zeros(nGridCapital,nGridProductivity)
+    mValueFunctionNew = zeros(nGridCapital,nGridProductivity)
+    mPolicyFunction   = zeros(nGridCapital,nGridProductivity)
+    expectedValueFunction = zeros(nGridCapital,nGridProductivity)
+
+    # 4. We pre-build output for each point in the grid
+
+    mOutput = (vGridCapital.^aalpha)*vProductivity;
+
+    # 5. Main iteration
+
+    maxDifference = 10.0
+    tolerance = 0.0000001
+    iteration = 0
+
+    @inbounds while(maxDifference > tolerance)
+        # expectedValueFunction = mValueFunction*mTransition';
+        A_mul_Bc!(expectedValueFunction, mValueFunction, mTransition)
+
+        for nProductivity = 1:nGridProductivity
+
+            # We start from previous choice (monotonicity of policy function)
+            gridCapitalNextPeriod = 1
+
+            for nCapital = 1:nGridCapital
+
+                valueHighSoFar = -1000.0
+                capitalChoice  = vGridCapital[1]
+
+                tmp = mOutput[nCapital,nProductivity]
+                for nCapitalNextPeriod = gridCapitalNextPeriod:nGridCapital
+
+                    consumption = tmp - vGridCapital[nCapitalNextPeriod]
+                    valueProvisional = (1 - bbeta) * mylog(consumption) +
+                        bbeta * expectedValueFunction[nCapitalNextPeriod,nProductivity]
+
+                    if (valueProvisional > valueHighSoFar)
+                	   valueHighSoFar = valueProvisional
+                    	capitalChoice = vGridCapital[nCapitalNextPeriod]
+                    	gridCapitalNextPeriod = nCapitalNextPeriod
+                    else
+                    	break # We break when we have achieved the max
+                    end
+
+                end
+
+                mValueFunctionNew[nCapital,nProductivity] = valueHighSoFar
+                mPolicyFunction[nCapital,nProductivity] = capitalChoice
+
+            end
+
+        end
+
+        maxDifference     = evaluate(Chebyshev(), mValueFunctionNew, mValueFunction)
+        # Swap the pointers
+        mTmp              = mValueFunction
+        mValueFunction    = mValueFunctionNew
+        mValueFunctionNew = mTmp
+        fill!(mValueFunctionNew, 0)
+
+        iteration += 1
+        if mod(iteration,10) == 0 || iteration == 1
+            println(" Iteration = ", iteration, " Sup Diff = ", maxDifference)
+        end
+
+    end
+
+    println(" Iteration = ", iteration, " Sup Diff = ", maxDifference)
+    println(" ")
+    println(" My check = ", mPolicyFunction[1000,3])
+
+end
+if !isinteractive()
+    main() # warmup()
+    if isempty(ARGS)
+        @time main()
+    elseif ARGS[1] == "--profile"
+        @profile main()
+        Profile.print(format = :flat, sortedby = :count)
+    end
+end
+
