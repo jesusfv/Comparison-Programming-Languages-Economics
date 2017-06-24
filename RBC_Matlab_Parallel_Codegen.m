81bc610c060498c93462ec4cce0cafa251aa8bdc
tholden <thomas.holden@gmail.com>
2017-06-24 15:37:14 +0100

    parallel codegen files

---
+++ RBC_Matlab_Parallel_Codegen.m
@@ -0,0 +1,60 @@
+%% Basic RBC model with full depreciation
+%
+% Jesus Fernandez-Villaverde
+% Haverford, July 3, 2013
+
+%% 0. Housekeeping
+
+warning( 'off', 'MATLAB:lang:cannotClearExecutingFunction' );
+clear all %#ok<CLALL>
+warning( 'on', 'MATLAB:lang:cannotClearExecutingFunction' );
+close all
+clc
+
+fprintf( 'Initial compilation.\n' );
+RBC_Matlab_Parallel_Perform_Codegen;
+fprintf( 'Compilation finished.\n\n' );
+
+tic
+
+%%  1. Calibration
+
+aalpha = 1/3;     % Elasticity of output w.r.t. capital
+bbeta  = 0.95;    % Discount factor
+
+% Productivity values
+vProductivity = [0.9792; 0.9896; 1.0000; 1.0106; 1.0212]';
+
+% Transition matrix
+mTransition   = [0.9727, 0.0273, 0.0000, 0.0000, 0.0000;
+                 0.0041, 0.9806, 0.0153, 0.0000, 0.0000;
+                 0.0000, 0.0082, 0.9837, 0.0082, 0.0000;
+                 0.0000, 0.0000, 0.0153, 0.9806, 0.0041;
+                 0.0000, 0.0000, 0.0000, 0.0273, 0.9727];
+             
+[ vGridCapital, mValueFunction, mPolicyFunction ] = RBC_Matlab_Get_Value_And_Policy_Functions_Parallel_mex( aalpha, bbeta, vProductivity, mTransition );
+
+toc
+
+%% 6. Plotting results
+
+figure(1)
+
+subplot(3,1,1)
+plot(vGridCapital,mValueFunction)
+xlim([vGridCapital(1) vGridCapital(end)])
+title('Value Function')
+
+subplot(3,1,2)
+plot(vGridCapital,mPolicyFunction)
+xlim([vGridCapital(1) vGridCapital(end)])
+title('Policy Function')
+
+vExactPolicyFunction = aalpha*bbeta.*(vGridCapital.^aalpha);
+
+subplot(3,1,3)
+plot((100.*(vExactPolicyFunction'-mPolicyFunction(:,3))./mPolicyFunction(:,3)))
+title('Comparison of Exact and Approximated Policy Function')
+
+%set(gcf,'PaperOrientation','landscape','PaperPosition',[-0.9 -0.5 12.75 9])
+%print('-dpdf','Figure1.pdf')
---
+++ RBC_Matlab_Parallel_Perform_Codegen.m
@@ -0,0 +1,44 @@
+% RBC_MATLAB_PARALLEL_PERFORN_CODEGEN   Generate MEX-function
+%  RBC_Matlab_Get_Value_And_Policy_Functions_mex from
+%  RBC_Matlab_Get_Value_And_Policy_Functions.
+% 
+% Script generated from project 'RBC_Matlab_Get_Value_And_Policy_Functions.prj'
+%  on 24-Jun-2017.
+% 
+% See also CODER, CODER.CONFIG, CODER.TYPEOF, CODEGEN.
+
+%% Create configuration object of class 'coder.MexCodeConfig'.
+cfg = coder.config('mex');
+cfg.EnableMemcpy = false;
+cfg.InitFltsAndDblsToZero = false;
+cfg.EnableOpenMP = false;
+cfg.CustomSourceCode = sprintf('#define muDoubleScalarIsNaN( x ) false\n#define muDoubleScalarIsInf( x ) false');
+cfg.MATLABSourceComments = true;
+cfg.ConstantInputs = 'Remove';
+cfg.GenerateReport = true;
+cfg.LaunchReport = false;
+cfg.ReportPotentialDifferences = false;
+cfg.ConstantFoldingTimeout = 2147483647;
+cfg.CompileTimeRecursionLimit = 2147483647;
+cfg.DynamicMemoryAllocation = 'AllVariableSizeArrays';
+cfg.SaturateOnIntegerOverflow = false;
+cfg.InlineThreshold = 2147483647;
+cfg.InlineThresholdMax = 2147483647;
+cfg.InlineStackLimit = 2147483647;
+cfg.IntegrityChecks = false;
+cfg.ResponsivenessChecks = false;
+cfg.ExtrinsicCalls = false;
+cfg.GlobalDataSyncMethod = 'NoSync';
+
+%% Define argument types for entry-point
+%  'RBC_Matlab_Get_Value_And_Policy_Functions'.
+ARGS = cell(1,1);
+ARGS{1} = cell(4,1);
+ARGS{1}{1} = coder.typeof(0);
+ARGS{1}{2} = coder.typeof(0);
+ARGS{1}{3} = coder.typeof(0,[1 5]);
+ARGS{1}{4} = coder.typeof(0,[5 5]);
+
+%% Invoke MATLAB Coder.
+codegen -config cfg RBC_Matlab_Get_Value_And_Policy_Functions_Parallel -args ARGS{1}
+
